9f10fe262550e77ed0d62d4d47cf7eb555fd8918
JM <jojomista69@gmail.com>
2025-06-16 11:00:15 +0800

    Fix chatContainer message render

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,10 +1,10 @@
 import { useChatStore } from "../store/useChatStore";
+import { useAuthStore } from "../store/useAuthStore";
 import { useEffect, useRef } from "react";
 
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
-import { useAuthStore } from "../store/useAuthStore";
 import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
@@ -17,20 +17,31 @@ const ChatContainer = () => {
     unsubscribeFromMessages,
   } = useChatStore();
   const { authUser } = useAuthStore();
-  const messageEndRef = useRef(null);
+
+  const messageContainerRef = useRef(null);
+  const isInitialLoadRef = useRef(true); 
 
   useEffect(() => {
-    getMessages(selectedUser._id);
+    if (!selectedUser?._id) return;
 
+    getMessages(selectedUser._id);
     subscribeToMessages();
 
+    isInitialLoadRef.current = true;
+
     return () => unsubscribeFromMessages();
-  }, [selectedUser._id, getMessages, subscribeToMessages, unsubscribeFromMessages]);
+  }, [selectedUser._id]);
 
   useEffect(() => {
-    if (messageEndRef.current && messages) {
-      messageEndRef.current.scrollIntoView({ behavior: "smooth" });
-    }
+    const timeout = setTimeout(() => {
+      const container = messageContainerRef.current;
+      if (container) {
+        container.scrollTop = container.scrollHeight;
+        isInitialLoadRef.current = false;
+      }
+    }, 0); 
+
+    return () => clearTimeout(timeout);
   }, [messages]);
 
   if (isMessagesLoading) {
@@ -43,18 +54,22 @@ const ChatContainer = () => {
     );
   }
 
+  const orderedMessages = [...messages].reverse();
+
   return (
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader />
 
-      <div className="flex-1 overflow-y-auto p-4 space-y-4 bg-base-100">
-        {messages.map((message) => {
+      <div
+        ref={messageContainerRef}
+        className="flex-1 overflow-y-auto p-4 space-y-4 bg-base-100"
+      >
+        {orderedMessages.map((message) => {
           const isSent = message.senderId === authUser._id;
           return (
             <div
               key={message._id}
               className={`chat ${isSent ? "chat-end" : "chat-start"}`}
-              ref={messageEndRef}
             >
               <div className="chat-image avatar">
                 <div className="size-10 rounded-full border">
@@ -99,6 +114,6 @@ const ChatContainer = () => {
       <MessageInput />
     </div>
   );
-
 };
+
 export default ChatContainer;
